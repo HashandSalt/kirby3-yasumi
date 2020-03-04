d0717b982b592d0a03089179577963de7c4c51a9
nilshoerrmann <nils.hoerrmann@hananils.de>
2020-03-04 10:47:36 +0100

    Add holiday collection

--- index.php
+++ index.php
@@ -4,11 +4,36 @@
 
 Kirby::plugin('hashsandsalt/kirby3-yasumi', [
 
-  'siteMethods' => [
-    'holidays' => function ($country, $year) {
-      $holidays = Yasumi\Yasumi::create($country, $year);
-      return $holidays;
-    }
-  ],
+    'siteMethods' => [
+        'holidays' => function ($country, $year, $locale = 'en_US') {
+            $holidays = Yasumi\Yasumi::create($country, $year, $locale);
+            return $holidays;
+        }
+    ],
+
+    'collections' => [
+        'holidays' => function ($site) {
+            $country = option('hashandsalt.kirby3-yasumi.country', 'Germany/LowerSaxony');
+            $year = option('hashandsalt.kirby3-yasumi.year', date('Y'));
+            $locale = option('hashandsalt.kirby3-yasumi.locale', 'de_DE');
+
+            foreach ($site->holidays($country, $year, $locale) as $holiday) {
+                $date = new DateTime($holiday);
+
+                $pages[] = [
+                    'slug' => $holiday->shortName,
+                    'num' => $date->format('Ymd'),
+                    'model' => 'holiday',
+                    'content' => [
+                        'title' => $holiday->getName(),
+                        'date' => $date->format('Y-m-d'),
+                        'type' => $holiday->getType()
+                    ]
+                ];
+            }
+
+            return Pages::factory($pages);
+        }
+    ]
 
 ]);
